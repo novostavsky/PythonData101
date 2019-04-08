a9d590605748f8019b65c96c2947fc05518dcc50
novostavsky <novostavsky@gmail.com>
2019-04-08 14:59:31 +0300

    yapf review

--- download_contributors_avatars.py
+++ download_contributors_avatars.py
@@ -10,6 +10,7 @@ import argparse
 BASE_URL = 'https://api.github.com/repos/{}/{}/contributors'
 DEFAULT_CHUNK_SIZE = 128
 
+
 #get a dictionary contributor:avatar_url for a github project
 def get_user_avatar_dict(user_login, project_name):
     user_avatar_dict = {}
@@ -21,6 +22,7 @@ def get_user_avatar_dict(user_login, project_name):
 
     return user_avatar_dict
 
+
 #save avatar profiles from a project (project owner and project name)
 #saving by chunks of DEFAULT_CHUNK_SIZE
 def save_avatar_into_folder(owner_name, user_name, project_name, avatar_url):
@@ -29,11 +31,12 @@ def save_avatar_into_folder(owner_name, user_name, project_name, avatar_url):
 
     response = requests.get(avatar_url, stream=True)
     file_type = response.headers['Content-Type'].split('/')[1]
-    file_name = '{}/{}.{}'.format(folder_name,user_name,file_type)
+    file_name = '{}/{}.{}'.format(folder_name, user_name, file_type)
 
     with open(file_name, 'wb') as file_im:
-        for chunk in response.iter_content(chunk_size = DEFAULT_CHUNK_SIZE):
+        for chunk in response.iter_content(chunk_size=DEFAULT_CHUNK_SIZE):
             file_im.write(chunk)
+
 
 #get token from a file if you want to use authentification
 def get_github_token(file_name):
@@ -41,13 +44,13 @@ def get_github_token(file_name):
         token = token_file.read()
     return token
 
+
 ####################################################################
 #################### let's roll#####################################
 ####################################################################
 parser = argparse.ArgumentParser(
     description='This is program that downloads avatars of contributors'
-                'for a github project.'
-)
+    'for a github project.')
 
 parser.add_argument('-u', "--user", action='store')
 parser.add_argument('-p', "--project", action='store')
@@ -56,5 +59,5 @@ args = parser.parse_args()
 user_avatar_dict = get_user_avatar_dict(args.user, args.project)
 
 for contributor in user_avatar_dict:
-    save_avatar_into_folder(args.user, contributor, args.project, user_avatar_dict[contributor])
-    
+    save_avatar_into_folder(args.user, contributor, args.project,
+                            user_avatar_dict[contributor])
